b660e77f59f76ee8ef59a2253d55ce19bf4718dd
deep <deep@unibot.app>
2024-12-12 11:23:49 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "tailwindcss";
-import commonjs from "@rollup/plugin-commonjs";
 
 // https://vite.dev/config/
 export default defineConfig({
