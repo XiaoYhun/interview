1924900b9e29404a41f66388f8757e31a08ff9d6
deep <deep@unibot.app>
2024-12-12 10:57:06 +0700

    fix build error

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "tailwindcss";
-import commonjs from "vite-plugin-commonjs";
+import commonjs from "@rollup/plugin-commonjs";
 
 // https://vite.dev/config/
 export default defineConfig({
